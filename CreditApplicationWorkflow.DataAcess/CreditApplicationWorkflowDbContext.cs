8e9db3e6c88303f941e0cff01295bcda56d128b1
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-05-15 09:24:14 +0200

    Add test data seed

--- CreditApplicationWorkflow.DataAcess/CreditApplicationWorkflowDbContext.cs
+++ CreditApplicationWorkflow.DataAcess/CreditApplicationWorkflowDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using CreditApplicationWorkflow.DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace CreditApplicationWorkflow.DataAccess
 {
@@ -16,5 +17,58 @@ namespace CreditApplicationWorkflow.DataAccess
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // seed data
+            modelBuilder.Entity<Customer>().HasData(new Customer
+            {
+                Id = 1,
+                CustomerName = "ABC Sp. z o.o."
+            });
+
+            modelBuilder.Entity<Department>().HasData(new Department
+            {
+                Id = 1,
+                DepartmentName = "Control"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee()
+            {
+                Id = 1,
+                FirstName = "Adam",
+                LastName = "Abacki",
+                DepartmentId = 1
+            });
+
+            modelBuilder.Entity<ProductType>().HasData(new ProductType()
+            {
+                Id = 1,
+                ProductTypeName = "Overdraft"
+            });
+
+            modelBuilder.Entity<ApplicationStatus>().HasData(new ApplicationStatus()
+            {
+                Id = 1,
+                ApplicationStatusName = "Initial check"
+            });
+
+            modelBuilder.Entity<CreditApplication>().HasData(new CreditApplication
+            {
+                Id = 1, 
+                DateOfSubmission = DateTime.Today,
+                CustomerId = 1,
+                ProductTypeId = 1,
+                AmountRequested = 100000M,
+                AmountGranted = 50000M,
+                ApplicationStatusId = 1,
+                DateOfLastStatusChange = DateTime.Today,
+                EmployeeId = 1,
+                Notes = string.Empty,
+                IsActive = true
+            });
+        }
     }
 }
--- CreditApplicationWorkflow.DataAcess/Migrations/20210515072306_DataSeed.Designer.cs
+++ CreditApplicationWorkflow.DataAcess/Migrations/20210515072306_DataSeed.Designer.cs
@@ -4,14 +4,16 @@ using CreditApplicationWorkflow.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CreditApplicationWorkflow.DataAccess.Migrations
 {
     [DbContext(typeof(CreditApplicationWorkflowDbContext))]
-    partial class CreditApplicationWorkflowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210515072306_DataSeed")]
+    partial class DataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
