5c3bf8e0f22685613f167eddfb48dab6f0b08c5d
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-05-16 11:12:43 +0200

    Add application services project

---
+++ CreditApplicationSystem.ApplicationServices/API/Domain/Models/CreditApplication.cs
@@ -0,0 +1,9 @@
+﻿namespace CreditApplicationSystem.ApplicationServices.API.Domain.Models
+{
+    public class CreditApplication
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal AmountRequested { get; set; }
+    }
+}
