ac7c6dcb796abfb8fce363480547c2d57ecd48fa
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-05-16 11:01:12 +0200

    Add web api controllers

---
+++ CreditApplicationSystem.WebApi/Controllers/CreditApplicationController.cs
@@ -0,0 +1,31 @@
+﻿using CreditApplicationSystem.DataAccess.Entities;
+using CreditApplicationSystem.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CreditApplicationSystem.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CreditApplicationController : ControllerBase
+    {
+        private readonly IRepository<CreditApplication>  _creditApplicationRepository;
+
+        public CreditApplicationController(IRepository<CreditApplication> creditApplicationRepository)
+        {
+            _creditApplicationRepository = creditApplicationRepository;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<CreditApplication> GetAllCreditApplications() => _creditApplicationRepository.GetAll();
+
+        [HttpGet]
+        [Route("{id}")]
+        public CreditApplication GetCreditApplicationById(int id)
+        {
+            return _creditApplicationRepository.GetById(id);
+        }
+    }
+}
---
+++ CreditApplicationSystem.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using CreditApplicationSystem.DataAccess.Entities;
+using CreditApplicationSystem.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace CreditApplicationSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepository<Customer> _repository;
+
+        public CustomerController(IRepository<Customer> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> GetAllCustomers() => _repository.GetAll();
+
+        [HttpGet]
+        [Route("{id}")]
+        public Customer GetCustomerById(int id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
