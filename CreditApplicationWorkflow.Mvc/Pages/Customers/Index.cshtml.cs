1594b94c6396155041f2bdabb23a740c62cde5f2
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-06-01 20:58:18 +0200

    Add customers razor view

---
+++ CreditApplicationWorkflow.Mvc/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreditApplicationWorkflow.Mvc.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
