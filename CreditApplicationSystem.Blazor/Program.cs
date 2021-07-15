769db382aa6600968b171fe535a372e92721555e
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-15 09:05:34 +0200

    Redister web app services

--- CreditApplicationSystem.Blazor/Program.cs
+++ CreditApplicationSystem.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using CreditApplicationSystem.Blazor.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -12,6 +13,13 @@ namespace CreditApplicationSystem.Blazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services
+                .AddScoped<IAuthenticationService, AuthenticationService>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<ICreditApplicationsService, CreditApplicationsService>()
+                .AddScoped<IHttpService, HttpService>()
+                .AddScoped<ILocalStorageService, LocalStorageService>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
