a574025e4d0c3a47830503e45e56c170439164f9
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-23 08:03:05 +0200

    Minor improvements

--- CreditApplicationWorkflow.Mvc/Pages/Customers/Index.cshtml.cs
+++ CreditApplicationWorkflow.Mvc/Pages/Customers/Index.cshtml.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CreditApplicationWorkflow.Mvc.Pages.Customers
@@ -13,13 +12,14 @@ namespace CreditApplicationWorkflow.Mvc.Pages.Customers
     [Authorize]
     public class IndexModel : PageModel
     {
-        public List<Customer> Customers { get; set; } = new List<Customer>();
         private readonly IMediator _mediator;
 
         public IndexModel(IMediator mediator)
         {
             _mediator = mediator;
         }
+
+        public List<Customer> Customers { get; private set; }
 
         public async Task OnGet([FromQuery] GetCustomersRequest request)
         {
