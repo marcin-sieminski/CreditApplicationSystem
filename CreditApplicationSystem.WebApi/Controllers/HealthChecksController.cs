0674e96a313b2daeca7a1100fe0539764bb273c1
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-29 21:47:31 +0200

    Add health check controller

---
+++ CreditApplicationSystem.WebApi/Controllers/HealthChecksController.cs
@@ -0,0 +1,25 @@
+﻿using CreditApplicationSystem.ApplicationServices.API.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CreditApplicationSystem.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/hc")]
+    public class HealthChecksController : ControllerBase
+    {
+        /// <summary>
+        /// Health check endpoint.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
+        public async Task<ActionResult<string>> GetAsync()
+        {
+            return "Healthy";
+        }
+    }
+}
