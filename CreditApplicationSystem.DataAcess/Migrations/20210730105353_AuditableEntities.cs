1661da8dbb56b9d5f160e85fbc9bb630b77d6fb0
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-30 13:03:18 +0200

    Update database concerning auditable emtities

---
+++ CreditApplicationSystem.DataAcess/Migrations/20210730105353_AuditableEntities.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CreditApplicationSystem.DataAccess.Migrations
+{
+    public partial class AuditableEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ProductTypes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ProductTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "ProductTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "ProductTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ProductTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ProductTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "ProductTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Employees",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Employees",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Departments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Departments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Departments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Departments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Customers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "CreditApplications",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "CreditApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "CreditApplications",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "CreditApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "CreditApplications",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "CreditApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "CreditApplications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ApplicationStatus",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ApplicationStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "ApplicationStatus",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "ApplicationStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ApplicationStatus",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ApplicationStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "ApplicationStatus",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "CreditApplications",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateOfLastStatusChange", "DateOfSubmission" },
+                values: new object[] { new DateTime(2021, 7, 30, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 30, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "CreditApplications");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ApplicationStatus");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ApplicationStatus");
+
+            migrationBuilder.UpdateData(
+                table: "CreditApplications",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateOfLastStatusChange", "DateOfSubmission" },
+                values: new object[] { new DateTime(2021, 7, 11, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 11, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
