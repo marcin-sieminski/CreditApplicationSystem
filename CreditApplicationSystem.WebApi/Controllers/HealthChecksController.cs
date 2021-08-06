2dbce06e73fa79e89c33df6336df8d64ac8f695b
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-08-06 11:45:42 +0200

    Improve health check logic for web api

--- CreditApplicationSystem.WebApi/Controllers/HealthChecksController.cs
+++ CreditApplicationSystem.WebApi/Controllers/HealthChecksController.cs
@@ -1,6 +1,10 @@
 ﻿using CreditApplicationSystem.ApplicationServices.API.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CreditApplicationSystem.WebApi.Controllers
@@ -9,6 +13,13 @@ namespace CreditApplicationSystem.WebApi.Controllers
     [Route("api/hc")]
     public class HealthChecksController : ControllerBase
     {
+        private readonly HttpClient _client = new();
+
+        //public HealthChecksController(HttpClient client)
+        //{
+        //    _client = client;
+        //}
+
         /// <summary>
         /// Health check endpoint.
         /// </summary>
@@ -17,9 +28,25 @@ namespace CreditApplicationSystem.WebApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
-        public async Task<ActionResult<string>> GetAsync()
+        public async Task<ActionResult<string>> CheckHealthAsync()
         {
-            return "Healthy";
+            var url = "https://localhost:5001/api/customers/";
+            var stopwatch = new Stopwatch();
+            
+            stopwatch.Start();
+            var response = await _client.GetAsync(url);
+            stopwatch.Stop();
+
+            if (response.IsSuccessStatusCode && stopwatch.ElapsedMilliseconds < 2000)
+            {
+                return $"Service is running (response: {stopwatch.ElapsedMilliseconds} milliseconds).";
+            }
+            else if (response.IsSuccessStatusCode)
+            {
+                return $"Service is running, but performance is degraded (response: {stopwatch.ElapsedMilliseconds} milliseconds).";
+            }
+
+            return "Service is not responding correctly";
         }
     }
 }
