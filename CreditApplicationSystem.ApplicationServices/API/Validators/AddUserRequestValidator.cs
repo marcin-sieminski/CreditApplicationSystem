bef2f9cf5af3804d1a073d1393f4f1f4ccbfc110
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-08-12 22:44:30 +0200

    Add validation for add user request

---
+++ CreditApplicationSystem.ApplicationServices/API/Validators/AddUserRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using CreditApplicationSystem.ApplicationServices.API.Domain.Users;
+using CreditApplicationSystem.DataAccess;
+using FluentValidation;
+using System.Linq;
+
+namespace CreditApplicationSystem.ApplicationServices.API.Validators
+{
+    public class AddUserRequestValidator : AbstractValidator<AddUserRequest>
+    {
+        private readonly CreditApplicationWorkflowDbContext _context;
+
+        public AddUserRequestValidator(CreditApplicationWorkflowDbContext dbContext)
+        {
+            _context = dbContext;
+
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            
+            RuleFor(x => x.Password).MinimumLength(6);
+            
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password);
+            
+            RuleFor(x => x.Email).Custom((value, context) =>
+            {
+                var emailInUse = _context.Users.Any(u => u.Email == value);
+                if (emailInUse)
+                {
+                    context.AddFailure("Email", "Provided email is taken");
+                }
+            });
+        }
+    }
+}
