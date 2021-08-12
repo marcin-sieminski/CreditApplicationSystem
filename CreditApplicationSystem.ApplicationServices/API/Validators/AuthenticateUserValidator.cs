ab1343aa5a8de08bd08df654a773e11caaa53beb
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-08-12 23:17:32 +0200

    Add authenticate user validator

---
+++ CreditApplicationSystem.ApplicationServices/API/Validators/AuthenticateUserValidator.cs
@@ -0,0 +1,15 @@
+﻿using CreditApplicationSystem.ApplicationServices.API.Domain.Users;
+using FluentValidation;
+
+namespace CreditApplicationSystem.ApplicationServices.API.Validators
+{
+    public class AuthenticateUserValidator : AbstractValidator<ValidateUserRequest>
+    {
+        public AuthenticateUserValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty();
+            
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
