d62544c0df798147b12b616ba7eebe9c5c064eb5
Marcin Siemiński <marcin.sieminski@outlook.com>
2022-01-16 15:35:12 +0100

    Add fluent validation for edit credit application request command with validation behaviour

---
+++ CreditApplicationSystem.ApplicationServices/API/Validators/EditCreditApplicationRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CreditApplicationSystem.ApplicationServices.API.Domain.CreditApplication;
+using FluentValidation;
+
+namespace CreditApplicationSystem.ApplicationServices.API.Validators;
+
+public class EditCreditApplicationRequestValidator : AbstractValidator<EditCreditApplicationRequest>
+{
+    public EditCreditApplicationRequestValidator()
+    {
+        RuleFor(x => x.AmountRequested).GreaterThan(0);
+        RuleFor(x => x.DateOfSubmission).GreaterThanOrEqualTo(new DateTime(2022, 1, 1));
+        RuleFor(x => x.DateOfLastStatusChange).GreaterThanOrEqualTo(new DateTime(2022, 1, 1));
+    }
+}
---
+++ CreditApplicationSystem.ApplicationServices/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreditApplicationSystem.ApplicationServices.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var failure = _validators.Select(x => x.Validate(context)).SelectMany(result => result.Errors)
+                    .Where(f => f != null).ToList();
+                if (failure.Any())
+                {
+                    throw new Exception();
+                }
+            }
+
+            return await next();
+        }
+    }
+}
