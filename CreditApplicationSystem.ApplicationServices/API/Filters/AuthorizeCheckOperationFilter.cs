5f44d1ffe73d8f8134876cc76881154a64b89107
Marcin Siemiński <marcin.sieminski@outlook.com>
2022-02-22 22:23:46 +0100

    Clean up

---
+++ CreditApplicationSystem.ApplicationServices/API/Filters/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CreditApplicationSystem.ApplicationServices.API.Filters;
+
+public class AuthorizeCheckOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+            
+    }
+}
